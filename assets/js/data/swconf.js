21ba6d11518910a4b29d3cca3fa90d5a57fcf90d
JiWoo Nam <woowoo4599@gmail.com>
2025-05-18 16:52:06 +0900

    add preview image 3D graphics

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1747552952',resources: [
+    cacheName: 'chirpy-1747554676',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
