c4cb02fbe3ed8940d4d2422f4d53ae4f9001efd6
JiWoo Nam <woowoo4599@gmail.com>
2025-05-18 16:23:48 +0900

    posting ch01 and ch01-1

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1746953641',resources: [
+    cacheName: 'chirpy-1747552952',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
